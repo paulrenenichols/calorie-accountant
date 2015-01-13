e88e37fb9aadfbe551897cc2dd269a459f2851a5
Paul Nichols <paul.rene.nichols@gmail.com>
2015-01-12 22:40:40 -0600

    Added first attempt at mcFridgeMagnets directive.

--- source/frontend/js/mc-fridge-magnets.js
+++ source/frontend/js/mc-fridge-magnets.js
@@ -12,4 +12,24 @@ angular.module('mcFridgeMagnets', [])
     service.splitStringIntoWordsAndPunctuation = splitStringIntoWordsAndPunctuation;
 
     return service;
+  }])
+  .directive('mcFridgeMagnets', ['mcFridgeMagnetsService', function(service) {
+    var directiveDefinition = {
+      scope: {
+        value: '='
+      },
+      restrict: 'E',
+      template: '<div ng-repeat="word in words track by $index" ng-bind="word"></div>',
+      link: function link(scope, iElement, iAttrs) {
+        scope.words = [];
+        scope.$watch('value', function () {
+          scope.words = service.splitStringIntoWordsAndPunctuation(scope.value);
+        });
+      }
+
+    };
+    return directiveDefinition;
+  }])
+  .controller('mcFridgeMagnetsTestCtrl', [function () {
+    this.text = "How are you doing, Tom? I'm doing fine.  Look out!";
   }]);
