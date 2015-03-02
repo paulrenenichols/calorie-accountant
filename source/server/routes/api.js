66694d03d3d9747609fe6a6f4f9434cc809ebe2c
Paul Nichols <paul.rene.nichols@gmail.com>
2015-03-01 20:40:25 -0600

    Fixed issue that was breaking server api.  req and res objects are circular, and the break JSON.stringify().  My bad.

--- source/server/routes/api.js
+++ source/server/routes/api.js
@@ -12,8 +12,6 @@ function addItem(item) {
 }
 
 router.post('/add-item', function(req, res) {
-  console.log('/add-item', 'req', JSON.stringify(req, null, 2));
-  console.log('/add-item', 'res', JSON.stringify(res, null, 2));
 
   addItem(req.body);
 
@@ -21,8 +19,7 @@ router.post('/add-item', function(req, res) {
 });
 
 router.get('/get-items', function(req, res) {
-  console.log('/get-items', 'req', JSON.stringify(req, null, 2));
-  console.log('/get-items', 'res', JSON.stringify(res, null, 2));
+  console.log('/get-items', JSON.stringify(items, null, 2));
 
   res.json(getItems());
 });
