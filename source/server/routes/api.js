847dc8c84d089269ff5c1fb5bb132a1018b3e435
Chuck McQuilkin <ctmcquilkin@gmail.com>
2015-03-22 16:07:47 -0500

    adding broken database interface code

--- source/server/routes/api.js
+++ source/server/routes/api.js
@@ -1,27 +1,64 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/calorie-accountant';
 var express = require('express');
 var router = express.Router();
 
-var items = [];
+var mongodb = null;
 
-function getItems() {
-  return items;
+MongoClient.connect(url, function(err, db) {
+  console.log('connected to server');
+  mongodb = db;
+});
+
+function addItemToDB(item, callback) {
+  var collection = mongodb.collection(items);
+  collection.insert(item, callback);
 }
 
-function addItem(item) {
-  items.unshift(item);
+function getItemsFromDB(callback) {
+  var collection = mongodb.collection(items);
+  collection.find({}).toArray(callback);
+}
+
+function getItems(callback) {
+  getItemsFromDB(callback);
+}
+
+function addItem(item, callback) {
+  addItemToDB(item, callback);
 }
 
 router.post('/add-item', function(req, res) {
 
-  addItem(req.body);
-
-  res.status(200).json({});
+  addItem(req.body, function(err, result) {
+    if (err) {
+      console.log('add items fail: ', err);
+      res.status(500).json({
+        error: err, 
+        message: 'insert failure' 
+      });
+    }
+    else {
+      res.status(200).json({});
+    }
+  });
 });
 
 router.get('/get-items', function(req, res) {
   console.log('/get-items', JSON.stringify(items, null, 2));
-
-  res.json(getItems());
+  getItems(function(err, items){
+    if (err) {
+      console.log('get items fail ', err);
+      res.status(500).json({
+        error: err,
+        message: 'get failure'
+      });
+    }
+    else {
+      console.log('items: ', items);
+      res.status(200).json(items);
+    }
+  });
 });
 
 module.exports = router;
