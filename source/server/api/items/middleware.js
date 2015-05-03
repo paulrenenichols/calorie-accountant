101e604b35a9f4722b579f4432d14ae2be447138
Paul Nichols <paul.rene.nichols@gmail.com>
2015-05-02 19:11:00 -0500

    Converted items api from a callback structure to a promise-based structure.

--- source/server/api/items/middleware.js
+++ source/server/api/items/middleware.js
@@ -1,39 +1,42 @@
+var Q = require('q');
 
 function middleware(db){
 
   var mw = {};
 
   function addItem(req, res) {
-    console.log('add item');
-    db.addItem(req.body, function(err, result) {
-      if (err) {
-        console.log('add items fail: ', err);
-        res.status(500).json({
-          error: err, 
-          message: 'insert failure' 
-        });
-      }
-      else {
-        res.status(200).json({});
-      }
-    });
+    console.log('mw addItem');
+    db.addItem(req.body)
+      .then(
+        function(result) {
+          res.status(200).json({});
+        },
+        function(reason) {
+          console.log('add items fail: ', reason);
+          res.status(500).json({
+            error: reason, 
+            message: 'insert failure' 
+          });
+        }
+      );
   }
 
   function getItems(req, res) {
-    console.log('/get-items');
-    db.getItems(function(err, items){
-      if (err) {
-        console.log('get items fail ', err);
+    console.log('mw getItems');
+    db.getItems().then(
+      function(items){
+        console.log('success, items: ', items);
+        console.log('items: ', items);
+        res.status(200).json(items);
+      },
+      function(reason) {
+        console.log('get items fail ', reason);
         res.status(500).json({
-          error: err,
+          error: reason,
           message: 'get failure'
         });
       }
-      else {
-        console.log('items: ', items);
-        res.status(200).json(items);
-      }
-    });
+    );
   }
 
   mw.addItem = addItem;
