b3f7b9f340f12ec15cf7b1ca26d7020e24fb66fb
Austin Nichols <greybubble@gmail.com>
2015-05-16 15:27:12 -0500

    Added route for mongo documents api

--- source/server/api/mongo/router.js
+++ source/server/api/mongo/router.js
@@ -8,10 +8,12 @@ function mongoRouter(mongodb) {
 
   var mongoDatabasesRouter = require('./databases/router')(mongodb);
   var mongoCollectionRouter = require('./collections/router')(mongodb);
+  var mongoDocumentsRouter = require('./documents/router')(mongodb);
 
 
   router.use('/databases', mongoDatabasesRouter);
   router.use('/collections', mongoCollectionRouter);
+  router.use('/collections', mongoDocumentsRouter);
 
   return router;
 }
