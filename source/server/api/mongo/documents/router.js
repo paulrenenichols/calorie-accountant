526611336720c58c638c0358501f65a5cc748bed
Austin Nichols <greybubble@gmail.com>
2015-05-17 15:55:30 -0500

    updated routes and added route for updateDocument.

--- source/server/api/mongo/documents/router.js
+++ source/server/api/mongo/documents/router.js
@@ -7,13 +7,15 @@ function mongoCollectionRouter(mongodb) {
   var database = require('./database')(mongodb);
   var middleware = require('./middleware')(database);
 
-  router.get('/:collection/documents', middleware.getDocuments);
+  router.get('/:collection/documents/', middleware.getDocuments);
 
   router.get('/:collection/documents/:key/:value', middleware.getDocuments);
 
-  router.post('/:collection/documents/:id', middleware.addDocument);
+  router.post('/:collection/documents/', middleware.addDocument);
 
-  router.delete('/:collection/documents/:id', middleware.removeDocument);
+  router.put('/:collection/documents/:key/:value', middleware.updateDocument);
+
+  router.delete('/:collection/documents/:key/:value', middleware.removeDocument);
 
   return router;
 }
